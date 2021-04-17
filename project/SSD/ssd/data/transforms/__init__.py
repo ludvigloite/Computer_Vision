c318ad80c2db8eb0729e6e01bdcc3e297cabd055
Håvard Roalsø Eiring <haavaei@oppdal.idi.ntnu.no>
2021-04-17 23:24:52 +0200

    resnext50 with random vertical flip

--- project/SSD/ssd/data/transforms/__init__.py
+++ project/SSD/ssd/data/transforms/__init__.py
@@ -8,11 +8,12 @@ def build_transforms(cfg, is_train=True):
         transform = [
             ConvertFromInts(),
             RandomMirror(),
+            RandomVerticalMirror(),
             ToPercentCoords(),
             RandomSampleCrop(), #added for task 4c
             Resize(cfg.INPUT.IMAGE_SIZE),
             SubtractMeans(cfg.INPUT.PIXEL_MEAN, cfg.INPUT.PIXEL_STD),
-            ToTensor(),
+            ToTensor(),          
         ]
     else:
         transform = [
