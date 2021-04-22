1806b6a4d5c759de4c4330abd429b1e8c27c9b5d
heiring <havard.eiring@gmail.com>
2021-04-22 12:06:38 +0200

    resnext18_600x600 minmax pxlstd outputs

--- project/SSD/ssd/data/transforms/__init__.py
+++ project/SSD/ssd/data/transforms/__init__.py
@@ -8,7 +8,7 @@ def build_transforms(cfg, is_train=True):
         transform = [
             ConvertFromInts(),
             RandomMirror(),
-            RandomSampleCrop(), #added for task 4c            
+            RandomSampleCrop(),         
             ToPercentCoords(),            
             Resize(cfg.INPUT.IMAGE_SIZE),
             SubtractMeans(cfg.INPUT.PIXEL_MEAN, cfg.INPUT.PIXEL_STD),
