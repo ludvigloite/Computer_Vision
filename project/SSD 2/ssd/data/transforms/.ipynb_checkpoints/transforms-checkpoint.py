bb13202bd95157cbbbbcda90c4a4740be9292553
ludvigloite <ludvigloite@gmail.com>
2021-04-28 07:22:18 +0200

    code delivered

---
+++ project/SSD 2/ssd/data/transforms/.ipynb_checkpoints/transforms-checkpoint.py
@@ -0,0 +1,466 @@
+# from https://github.com/amdegroot/ssd.pytorch
+import torch
+import cv2
+import numpy as np
+from numpy import random
+from torchvision.transforms import functional
+from .bbox_util import *
+
+
+
+def intersect(box_a, box_b):
+    max_xy = np.minimum(box_a[:, 2:], box_b[2:])
+    min_xy = np.maximum(box_a[:, :2], box_b[:2])
+    inter = np.clip((max_xy - min_xy), a_min=0, a_max=np.inf)
+    return inter[:, 0] * inter[:, 1]
+
+
+def jaccard_numpy(box_a, box_b):
+    """Compute the jaccard overlap of two sets of boxes.  The jaccard overlap
+    is simply the intersection over union of two boxes.
+    E.g.:
+        A ∩ B / A ∪ B = A ∩ B / (area(A) + area(B) - A ∩ B)
+    Args:
+        box_a: Multiple bounding boxes, Shape: [num_boxes,4]
+        box_b: Single bounding box, Shape: [4]
+    Return:
+        jaccard overlap: Shape: [box_a.shape[0], box_a.shape[1]]
+    """
+    inter = intersect(box_a, box_b)
+    area_a = ((box_a[:, 2] - box_a[:, 0]) *
+              (box_a[:, 3] - box_a[:, 1]))  # [A,B]
+    area_b = ((box_b[2] - box_b[0]) *
+              (box_b[3] - box_b[1]))  # [A,B]
+    union = area_a + area_b - inter
+    return inter / union  # [A,B]
+
+
+def remove_empty_boxes(boxes, labels):
+    """Removes bounding boxes of W or H equal to 0 and its labels
+
+    Args:
+        boxes   (ndarray): NP Array with bounding boxes as lines
+                           * BBOX[x1, y1, x2, y2]
+        labels  (labels): Corresponding labels with boxes
+
+    Returns:
+        ndarray: Valid bounding boxes
+        ndarray: Corresponding labels
+    """
+    del_boxes = []
+    for idx, box in enumerate(boxes):
+        if box[0] == box[2] or box[1] == box[3]:
+            del_boxes.append(idx)
+
+    return np.delete(boxes, del_boxes, 0), np.delete(labels, del_boxes)
+
+
+class Compose(object):
+    """Composes several augmentations together.
+    Args:
+        transforms (List[Transform]): list of transforms to compose.
+    Example:
+        >>> augmentations.Compose([
+        >>>     transforms.CenterCrop(10),
+        >>>     transforms.ToTensor(),
+        >>> ])
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img, boxes=None, labels=None):
+        for t in self.transforms:
+            img, boxes, labels = t(img, boxes, labels)
+            if boxes is not None:
+                boxes, labels = remove_empty_boxes(boxes, labels)
+        return img, boxes, labels
+
+
+class ConvertFromInts(object):
+    def __call__(self, image, boxes=None, labels=None):
+        return image.astype(np.float32), boxes, labels
+
+
+class SubtractMeans(object):
+    def __init__(self, mean, std):
+        self.mean = np.array(mean, dtype=np.float32)
+        self.std = np.array(std, dtype=np.float32) * 255
+
+    def __call__(self, image, boxes=None, labels=None):
+        image = image.astype(np.float32)
+
+        image = (image - self.mean) / self.std
+        return image.astype(np.float32), boxes, labels
+
+
+class ToAbsoluteCoords(object):
+    def __call__(self, image, boxes=None, labels=None):
+        height, width, channels = image.shape
+        boxes[:, 0] *= width
+        boxes[:, 2] *= width
+        boxes[:, 1] *= height
+        boxes[:, 3] *= height
+
+        return image, boxes, labels
+
+
+class ToPercentCoords(object):
+    def __call__(self, image, boxes=None, labels=None):
+        height, width, channels = image.shape
+        boxes[:, 0] /= width
+        boxes[:, 2] /= width
+        boxes[:, 1] /= height
+        boxes[:, 3] /= height
+
+        return image, boxes, labels
+
+
+class Resize(object):
+    def __init__(self, imshape):
+        w, h = imshape
+        self.shape = (w, h)
+
+    def __call__(self, image, boxes=None, labels=None):
+        image = cv2.resize(image, self.shape)
+        return image, boxes, labels
+
+
+class ToTensor(object):
+    def __call__(self, cvimage, boxes=None, labels=None):
+        return torch.from_numpy(cvimage.astype(np.float32)).permute(2, 0, 1), boxes, labels
+
+
+class RandomSampleCrop(object):
+    """Crop
+    Arguments:
+        img (Image): the image being input during training
+        boxes (Tensor): the original bounding boxes in pt form
+        labels (Tensor): the class labels for each bbox
+        mode (float tuple): the min and max jaccard overlaps
+    Return:
+        (img, boxes, classes)
+            img (Image): the cropped image
+            boxes (Tensor): the adjusted bounding boxes in pt form
+            labels (Tensor): the class labels for each bbox
+    """
+
+    def __init__(self):
+        self.sample_options = (
+            # using entire original input image
+            None,
+            # sample a patch s.t. MIN jaccard w/ obj in .1,.3,.4,.7,.9
+            (0.1, None),
+            (0.3, None),
+            (0.7, None),
+            (0.9, None),
+            # randomly sample a patch
+            (None, None),
+        )
+
+    def __call__(self, image, boxes=None, labels=None):
+        # guard against no boxes
+        if boxes is not None and boxes.shape[0] == 0:
+            return image, boxes, labels
+        height, width, _ = image.shape
+        while True:
+            # randomly choose a mode
+            mode = random.choice(self.sample_options)
+            if mode is None:
+                return image, boxes, labels
+
+            min_iou, max_iou = mode
+            if min_iou is None:
+                min_iou = float('-inf')
+            if max_iou is None:
+                max_iou = float('inf')
+
+            # max trails (50)
+            for _ in range(50):
+                current_image = image
+
+                w = random.uniform(0.3 * width, width)
+                h = random.uniform(0.3 * height, height)
+
+                # aspect ratio constraint b/t .5 & 2
+                if h / w < 0.5 or h / w > 2:
+                    continue
+
+                left = random.uniform(width - w)
+                top = random.uniform(height - h)
+
+                # convert to integer rect x1,y1,x2,y2
+                rect = np.array([int(left), int(top), int(left + w), int(top + h)])
+
+                # calculate IoU (jaccard overlap) b/t the cropped and gt boxes
+                overlap = jaccard_numpy(boxes, rect)
+
+                # is min and max overlap constraint satisfied? if not try again
+                if overlap.max() < min_iou or overlap.min() > max_iou:
+                    continue
+
+                # cut the crop from the image
+                current_image = current_image[rect[1]:rect[3], rect[0]:rect[2],
+                                :]
+
+                # keep overlap with gt box IF center in sampled patch
+                centers = (boxes[:, :2] + boxes[:, 2:]) / 2.0
+
+                # mask in all gt boxes that above and to the left of centers
+                m1 = (rect[0] < centers[:, 0]) * (rect[1] < centers[:, 1])
+
+                # mask in all gt boxes that under and to the right of centers
+                m2 = (rect[2] > centers[:, 0]) * (rect[3] > centers[:, 1])
+
+                # mask in that both m1 and m2 are true
+                mask = m1 * m2
+
+                # have any valid boxes? try again if not
+                if not mask.any():
+                    continue
+
+                # take only matching gt boxes
+                current_boxes = boxes[mask, :].copy()
+
+                # take only matching gt labels
+                current_labels = labels[mask]
+
+                # should we use the box left and top corner or the crop's
+                current_boxes[:, :2] = np.maximum(current_boxes[:, :2],
+                                                  rect[:2])
+                # adjust to crop (by substracting crop's left,top)
+                current_boxes[:, :2] -= rect[:2]
+
+                current_boxes[:, 2:] = np.minimum(current_boxes[:, 2:],
+                                                  rect[2:])
+                # adjust to crop (by substracting crop's left,top)
+                current_boxes[:, 2:] -= rect[:2]
+
+                return current_image, current_boxes, current_labels
+
+
+class Expand(object):
+    def __init__(self, mean):
+        self.mean = mean
+
+    def __call__(self, image, boxes, labels):
+        if random.randint(2):
+            return image, boxes, labels
+
+        height, width, depth = image.shape
+        ratio = random.uniform(1, 4)
+        left = random.uniform(0, width * ratio - width)
+        top = random.uniform(0, height * ratio - height)
+
+        expand_image = np.zeros(
+            (int(height * ratio), int(width * ratio), depth),
+            dtype=image.dtype)
+        expand_image[:, :, :] = self.mean
+        expand_image[int(top):int(top + height),
+        int(left):int(left + width)] = image
+        image = expand_image
+
+        boxes = boxes.copy()
+        boxes[:, :2] += (int(left), int(top))
+        boxes[:, 2:] += (int(left), int(top))
+
+        return image, boxes, labels
+
+
+class RandomMirror(object):
+    def __call__(self, image, boxes, classes):
+        _, width, _ = image.shape
+        if random.randint(2):
+            image = image[:, ::-1]
+            boxes = boxes.copy()
+            boxes[:, 0::2] = width - boxes[:, 2::-2]
+        return image, boxes, classes
+    
+
+class RandomVerticalMirror(object):
+    def __call__(self, image, boxes, classes):
+        height, _, _ = image.shape
+        if random.randint(2):
+            image = image[::-1, :]
+            boxes = boxes.copy()
+            boxes[:, 1::2] = height - boxes[:, 3::-2]
+        return image, boxes, classes
+
+class RandomRotate(object):
+    """Randomly rotates an image    
+    
+    
+    Bounding boxes which have an area of less than 25% in the remaining in the 
+    transformed image is dropped. The resolution is maintained, and the remaining
+    area if any is filled by black color.
+    
+    Parameters
+    ----------
+    angle: float or tuple(float)
+        if **float**, the image is rotated by a factor drawn 
+        randomly from a range (-`angle`, `angle`). If **tuple**,
+        the `angle` is drawn randomly from values specified by the 
+        tuple
+        
+    Returns
+    -------
+    
+    numpy.ndaaray
+        Rotated image in the numpy format of shape `HxWxC`
+    
+    numpy.ndarray
+        Tranformed bounding box co-ordinates of the format `n x 4` where n is 
+        number of bounding boxes and 4 represents `x1,y1,x2,y2` of the box
+        
+    """
+
+    def __init__(self, angle = 10):
+        self.angle = angle
+        
+        if type(self.angle) == tuple:
+            assert len(self.angle) == 2, "Invalid range"  
+            
+        else:
+            self.angle = (-self.angle, self.angle)
+            
+    def __call__(self, img, bboxes, classes):
+
+        if random.randint(10) < 6:
+            return img, bboxes, classes
+    
+        angle = random.uniform(*self.angle)
+    
+        w,h = img.shape[1], img.shape[0]
+        cx, cy = w//2, h//2
+    
+        img = rotate_im(img, angle)
+    
+        corners = get_corners(bboxes)
+    
+        corners = np.hstack((corners, bboxes[:,4:]))
+    
+    
+        corners[:,:8] = rotate_box(corners[:,:8], angle, cx, cy, h, w)
+    
+        new_bbox = get_enclosing_box(corners)
+    
+    
+        scale_factor_x = img.shape[1] / w
+    
+        scale_factor_y = img.shape[0] / h
+    
+        img = cv2.resize(img, (w,h))
+    
+        new_bbox[:,:4] /= [scale_factor_x, scale_factor_y, scale_factor_x, scale_factor_y] 
+    
+        bboxes  = new_bbox
+    
+        bboxes = clip_box(bboxes, [0,0,w, h], 0.25)
+    
+        return img, bboxes, classes
+
+    
+class RandomShear(object):
+    """Randomly shears an image in horizontal direction   
+    
+    
+    Bounding boxes which have an area of less than 25% in the remaining in the 
+    transformed image is dropped. The resolution is maintained, and the remaining
+    area if any is filled by black color.
+    
+    Parameters
+    ----------
+    shear_factor: float or tuple(float)
+        if **float**, the image is sheared horizontally by a factor drawn 
+        randomly from a range (-`shear_factor`, `shear_factor`). If **tuple**,
+        the `shear_factor` is drawn randomly from values specified by the 
+        tuple
+        
+    Returns
+    -------
+    
+    numpy.ndaaray
+        Sheared image in the numpy format of shape `HxWxC`
+    
+    numpy.ndarray
+        Tranformed bounding box co-ordinates of the format `n x 4` where n is 
+        number of bounding boxes and 4 represents `x1,y1,x2,y2` of the box
+        
+    """
+
+    def __init__(self, shear_factor = 0.2):
+        self.shear_factor = shear_factor
+        
+        if type(self.shear_factor) == tuple:
+            assert len(self.shear_factor) == 2, "Invalid range for scaling factor"   
+        else:
+            self.shear_factor = (-self.shear_factor, self.shear_factor)
+        
+        shear_factor = random.uniform(*self.shear_factor)
+        
+    def __call__(self, img, bboxes, classes):
+        
+        if random.randint(10) < 6:
+            return img, bboxes, classes
+    
+        shear_factor = random.uniform(*self.shear_factor)
+    
+        w,h = img.shape[1], img.shape[0]
+    
+        if shear_factor < 0:
+            img, bboxes = HorizontalFlip()(img, bboxes)
+    
+        M = np.array([[1, abs(shear_factor), 0],[0,1,0]])
+    
+        nW =  img.shape[1] + abs(shear_factor*img.shape[0])
+    
+        bboxes[:,[0,2]] += ((bboxes[:,[1,3]]) * abs(shear_factor) ).astype(int) 
+    
+    
+        img = cv2.warpAffine(img, M, (int(nW), img.shape[0]))
+    
+        if shear_factor < 0:
+        	img, bboxes = HorizontalFlip()(img, bboxes)
+    
+        img = cv2.resize(img, (w,h))
+    
+        scale_factor_x = nW / w
+    
+        bboxes[:,:4] /= [scale_factor_x, 1, scale_factor_x, 1] 
+    
+    
+        return img, bboxes, classes
+    
+
+class HorizontalFlip(object):
+
+    """Randomly horizontally flips the Image with the probability *p*
+    Parameters
+    ----------
+    p: float
+        The probability with which the image is flipped
+    Returns
+    -------
+    numpy.ndaaray
+        Flipped image in the numpy format of shape `HxWxC`
+    numpy.ndarray
+        Tranformed bounding box co-ordinates of the format `n x 4` where n is
+        number of bounding boxes and 4 represents `x1,y1,x2,y2` of the box
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, img, bboxes):
+        img_center = np.array(img.shape[:2])[::-1]/2
+        img_center = np.hstack((img_center, img_center))
+
+        img = img[:, ::-1, :]
+        bboxes[:, [0, 2]] += 2*(img_center[[0, 2]] - bboxes[:, [0, 2]])
+
+        box_w = abs(bboxes[:, 0] - bboxes[:, 2])
+
+        bboxes[:, 0] -= box_w
+        bboxes[:, 2] += box_w
+
+        return img, bboxes
---
+++ project/SSD 2/ssd/modeling/.ipynb_checkpoints/detector-checkpoint.py
@@ -0,0 +1,96 @@
+from torch import nn
+from ssd.modeling.backbone.vgg import VGG
+from ssd.modeling.backbone.basic import BasicModel
+from ssd.modeling.backbone.resnet18 import Model
+from ssd.modeling.backbone.resnext50 import ResNeXt50
+from ssd.modeling.backbone.resnet18_800x450 import ResNet18_800x450
+from ssd.modeling.backbone.resnet101 import ResNet101
+from ssd.modeling.backbone.resnet18_600x600 import ResNet18_600x600
+from ssd.modeling.backbone.resnet50_600x600 import ResNet50_600x600
+from ssd.modeling.backbone.resnext50_600x600 import ResNeXt50_600x600
+from ssd.modeling.backbone.resnext50_800x450 import ResNeXt50_800x450
+from ssd.modeling.backbone.inception import Inception
+from ssd.modeling.box_head.box_head import SSDBoxHead
+from ssd.utils.model_zoo import load_state_dict_from_url
+from ssd import torch_utils
+import importlib
+
+import torchvision.models as models
+
+
+class SSDDetector(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.backbone = build_backbone(cfg)
+        self.box_head = SSDBoxHead(cfg)
+        print(
+            "Detector initialized. Total Number of params: ",
+            f"{torch_utils.format_params(self)}")
+        print(
+            f"Backbone number of parameters: {torch_utils.format_params(self.backbone)}")
+        print(
+            f"SSD Head number of parameters: {torch_utils.format_params(self.box_head)}")
+
+    def forward(self, images, targets=None):
+        features = self.backbone(images)
+        detections, detector_losses = self.box_head(features, targets)
+        if self.training:
+            return detector_losses
+        return detections
+
+
+def build_backbone(cfg):
+    backbone_name = cfg.MODEL.BACKBONE.NAME
+
+    if backbone_name == "basic":
+        model = BasicModel(cfg)
+        return model
+    if backbone_name == "resNet":
+        model = Model(cfg)
+        return model
+    if backbone_name == "resNet18":
+        model = models.resnet18(cfg)
+        return model
+    if backbone_name == "resnet50_600x600":
+	    model = ResNet50_600x600(cfg)
+	    return model
+    if backbone_name == "resneXt50_600x600":
+	    model = ResNeXt50_600x600(cfg)
+	    return model
+    if backbone_name == "resnet18_600x600":
+        model = ResNet18_600x600(cfg)
+        return model
+    if backbone_name == "resnet50_800x450":
+        model = ResNet50_800x450(cfg)
+        return model
+    if backbone_name == "resnet18_800x450":
+        model = ResNet18_800x450(cfg)
+        return model
+    if backbone_name == "resnext50":
+        model = ResNeXt50(cfg)
+        return model
+    if backbone_name == "resnext50_800x450":
+        model = ResNeXt50_800x450(cfg)
+        return model
+    if backbone_name == "resnet101":
+        model = ResNet101(cfg)
+        return model
+    if backbone_name == "inception":
+        model = Inception(cfg)
+        return model
+    if backbone_name == "vgg":
+        model = VGG(cfg)
+        if cfg.MODEL.BACKBONE.PRETRAINED:
+            state_dict = load_state_dict_from_url(
+                "https://s3.amazonaws.com/amdegroot-models/vgg16_reducedfc.pth")
+            model.init_from_pretrain(state_dict)
+        return model
+    
+    """
+     if True:
+        model_path = 'ssd.modeling.backbone.{}'.format(backbone_name)
+        model = importlib.import_module(model_path).Model(cfg)
+        return model
+        """
+    
